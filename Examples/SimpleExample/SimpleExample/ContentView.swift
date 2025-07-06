40975471eeb593945c75c4945dbb65cfdd30412c
Duong Tuan <tuan.duong@enouvo.com>
2025-07-06 22:44:51 +0700

    init: Simple example to test CoreDataPackage

---
+++ Examples/SimpleExample/SimpleExample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SimpleExample
+//
+//  Created by Duong Tuan on 28/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Examples/SimpleExample/SimpleExample/SimpleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleExampleApp.swift
+//  SimpleExample
+//
+//  Created by Duong Tuan on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
