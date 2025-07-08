b9071d0ec05eefba925559fa04384d8c82353602
Duong Tuan <tuan.duong@enouvo.com>
2025-07-08 16:06:50 +0700

    update: Simple example with step3 - Integrate the managed object context into the Swift UI environment by adding a "environment" modifier to the view

--- Examples/SimpleExample/SimpleExample/SimpleExampleApp.swift
+++ Examples/SimpleExample/SimpleExample/SimpleExampleApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import CoreDataPackage
 
 @main
 struct SimpleExampleApp: App {
+    // Get the managed object context from the shared persistent container.
+    let viewContext = EcommerceDataManager.shared.mainContext
+    
     var body: some Scene {
         WindowGroup {
+            // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
+            // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
             ContentView()
+                .environment(\.managedObjectContext, viewContext)
         }
     }
 }
